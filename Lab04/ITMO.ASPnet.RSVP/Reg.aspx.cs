7948a249d1a13e8d7f6d15e564804d3005242c3a
Maria <m.tabakova12@gmail.com>
2022-03-10 15:07:40 +0300

    lab04

---
+++ Lab04/ITMO.ASPnet.RSVP/Reg.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITMO.ASPnet.RSVP
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) 
+            {
+                GuestResponse rsvp = new GuestResponse(name.Text, email.Text, phone.Text, CheckBoxYN.Checked);
+                ResponseRepository.GetRepository().AddResponse(rsvp);
+                if (rsvp.WillAttend.HasValue && rsvp.WillAttend.Value) 
+                {
+                    Response.Redirect("html_answer\\seeyouthere.html");
+                }
+                else 
+                {
+                    Response.Redirect("html_answer\\sorryyoucantcome.html"); 
+                }
+            }
+        }
+    }
+}
---
+++ Lab04/ITMO.ASPnet.RSVP/Start.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Start.aspx.cs" Inherits="ITMO.ASPnet.RSVP.Start" MasterPageFile ="~/Site.master" %>
+
+<asp:Content ID="MainContent" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+                <h1>Приглашение на семинар</h1> 
+                <p>Вы приглашены на наш семинар</p> 
+                <p>Подтвердите свое согласие, пройдя регистрацию</p>  
+
+</asp:Content>
+
