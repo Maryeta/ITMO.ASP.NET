b78ccaf773f6afde0f0e91e74aea9ba84f26cfae
Maria <m.tabakova12@gmail.com>
2022-03-15 21:09:04 +0300

    Lab02 MVC

---
+++ MVC_Lab02/ITMO.ASPnet.WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITMO.ASPnet.WebMVCR1.Models;
+
+namespace ITMO.ASPnet.WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static PersonRepository db = new PersonRepository();
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Дброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+
+        //public string Index(string hel) 
+        //{ 
+        //    int hour = DateTime.Now.Hour; 
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel; 
+        //    return Greeting;
+        //}
+
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+
+        public ViewResult OutputData()
+        {
+            ViewBag.Pers = db.GetAllResponses;
+            ViewBag.Count = db.NumberOfPerson;
+            return View("ListPerson");
+        }
+    }
+}
